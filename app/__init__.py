62e2326ade3500fa9896dbbfbb0b137667d83acf
Jocelyn Heckenkamp <jocelynheckenkamp@gmail.com>
2021-06-24 14:39:32 +0000

    VPS, Nginx, Sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 import os
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
+from . import db
 
 load_dotenv()
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
+## Rest of the file
+## Rest of the file
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/health')
+def health():
+    return render_template('health.html', title="Health", url=os.getenv("URL"))
 
 @app.route('/projects')
 def projects():
